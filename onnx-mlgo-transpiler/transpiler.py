70887c111dc7decec3c81f6f2f937416e7b4273c
Karan Handa <karan.handa.fox@gmail.com>
2024-02-07 21:50:17 +0530

    docs: create docstrings for util functions

--- onnx-mlgo-transpiler/transpiler.py
+++ onnx-mlgo-transpiler/transpiler.py
@@ -5,27 +5,15 @@ def read_from_onnx():
 
 def transpile(onnx, model_name):
   with open('test.go', 'w') as file:
-    # imports and boilerplate
     writer.create_go_boilerplate(file)
-
-    # type mnist_hparams
     writer.create_hparams_type(file, model_name)
-    
-    # type mnist_model
     writer.create_model_type(file, model_name)
-
-    # func mnist_model_load
     writer.create_weight_loading_func(file)
-
-    # func mnist_eval
     writer.create_eval_func(file)
-
-    # func TestMNIST
-    writer.create_main_func(file)
-  
+    writer.create_main_func(file)  
 
 def main():
   onnx_output = read_from_onnx()
-  transpile(onnx_output, 'mnist')
+  transpile(onnx_output, 'test')
   
 main()
--- onnx-mlgo-transpiler/writer.py
+++ onnx-mlgo-transpiler/writer.py
@@ -1,4 +1,7 @@
 def create_go_boilerplate(file):
+  """
+  create imports and boilerplate.
+  """
   # TODO: remove redundant imports
   file.write("""\
 package main
@@ -15,6 +18,10 @@ import (
   )
 
 def create_hparams_type(file, model_name: str):
+  """
+  create type for hparams.
+  e.g. - mnist_hparams
+  """
   # TODO: is this correct? are any variables required here?
   file.write(f"""\
 type {model_name}_hparams struct{{
@@ -27,7 +34,11 @@ type {model_name}_hparams struct{{
   )
 
 def create_model_type(file, model_name: str):
-  # TODO: in the transpilation, abstract the logic from the string manipulation output
+  """
+  create model type
+  e.g. - mnist_model
+  """
+  # TODO: abstract the logic from the string manipulation output
   # TODO: change these depending on the model weights and layers
   layers = """\
   fc1_weight *ml.Tensor;
@@ -49,10 +60,22 @@ type {model_name}_model struct {{
   )
 
 def create_weight_loading_func(file):
+  """
+  create function to load model weights
+  e.g. - mnist_model_load
+  """
   pass
 
 def create_eval_func(file):
+  """
+  create function to evaluate model
+  e.g. - mnist_eval
+  """
   pass
 
 def create_main_func(file):
+  """
+  create inference main function
+  e.g. TestMNIST 
+  """
   pass
