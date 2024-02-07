d1c0e37e7315e497b9c6d1eed52530a2e9088282
Karan Handa <karan.handa.fox@gmail.com>
2024-02-07 20:30:19 +0530

    feat: pass file to all util functions

--- onnx-mlgo-transpiler/transpiler.py
+++ onnx-mlgo-transpiler/transpiler.py
@@ -4,23 +4,22 @@ def read_from_onnx():
   pass
 
 def transpile(onnx):
-  
-  # type mnist_model
-  utils.create_model_type()
-  
-  # func mnist_model_load
-  utils.create_weight_loading_func()
-  
-  # func mnist_eval
-  utils.create_eval_func()
-  
-  # func TestMNIST
-  utils.create_main_func()
+  with open('test.go', 'w') as file:    
+    # type mnist_model
+    utils.create_model_type(file)
+
+    # func mnist_model_load
+    utils.create_weight_loading_func(file)
+
+    # func mnist_eval
+    utils.create_eval_func(file)
+    
+    # func TestMNIST
+    utils.create_main_func(file)
   
 
 def main():
   onnx_output = read_from_onnx()
-  
   transpile(onnx_output)
   
 main()
