212d4dca768a4e54611ebd63f3fbfd3b2cac86f8
Karan Handa <karan.handa.fox@gmail.com>
2024-02-11 21:14:10 +0530

    feat: remove model_name, cleanup and abstract layers

--- onnx-mlgo-transpiler/transpiler.py
+++ onnx-mlgo-transpiler/transpiler.py
@@ -11,6 +11,7 @@ def read_from_onnx():
 def transpile(onnx_model, model_name):
   # TODO: make sure that model_name is a valid file_name
   # TODO: get rid of everything that isn't required in this repository
+  # TODO: remove model_name from the entire transpiler. this is not required and just adds additional complexity. make the name default to 'model' and write the model_name at the beginning as a comment
 
   mlgo_model_path = Path('dist/')
 
@@ -19,11 +20,11 @@ def transpile(onnx_model, model_name):
   with open(mlgo_model_path / 'test.go', 'w') as file:
     writer.create_go_boilerplate(file)
     writer.create_model_utils(file)
-    writer.create_hparams_type(file, model_name)
-    writer.create_model_type(file, model_name)
-    writer.create_weight_loading_func(file, model_name)
-    writer.create_eval_func(file, model_name)
-    writer.create_main_func(file, model_name)
+    writer.create_hparams_type(file)
+    writer.create_model_type(file)
+    writer.create_weight_loading_func(file)
+    writer.create_eval_func(file)
+    writer.create_main_func(file)
 
 def main():
   onnx_model = read_from_onnx()
--- onnx-mlgo-transpiler/writer.py
+++ onnx-mlgo-transpiler/writer.py
@@ -46,14 +46,14 @@ func readInt(file *os.File) uint32 {
 """
   )
 
-def create_hparams_type(file, model_name: str):
+def create_hparams_type(file):
   """
   create type for hparams.
   e.g. - mnist_hparams
   """
   # TODO: is this correct? are any variables required here?
   file.write(f"""\
-type {model_name}_hparams struct{{
+type model_hparams struct{{
   n_input int32;
   n_hidden int32;
   n_classes int32;
@@ -62,7 +62,7 @@ type {model_name}_hparams struct{{
 """
   )
 
-def create_model_type(file, model_name: str):
+def create_model_type(file):
   """
   create model type
   e.g. - mnist_model
@@ -78,9 +78,9 @@ def create_model_type(file, model_name: str):
   """
 
   file.write(f"""\
-type {model_name}_model struct {{
+type model_struct struct {{
 
-  hparams {model_name}_hparams;
+  hparams model_hparams;
 
 {layers}
 
@@ -88,7 +88,7 @@ type {model_name}_model struct {{
 """
   )
 
-def create_weight_loading_func(file, model_name: str):
+def create_weight_loading_func(file):
   """
   create function to load model weights
   e.g. - mnist_model_load
@@ -97,7 +97,7 @@ def create_weight_loading_func(file, model_name: str):
   # TODO: abstract layers programmatically.
 
   file.write(f"""\
-func {model_name}_model_load(fname string, model *{model_name}_model) error {{
+func model_load(fname string, model *model_struct) error {{
 
   file, err := os.Open(fname)
   if err != nil {{
@@ -176,50 +176,48 @@ func {model_name}_model_load(fname string, model *{model_name}_model) error {{
 """
   )
 
-def create_eval_func(file, model_name: str):
+def create_eval_func(file):
   """
   create function to evaluate model
   e.g. - mnist_eval
   """
+  
+  layers = utils.create_layers()
 
   # TODO: create input tensor according to onnx
   # TODO: create fc's (layers) according to onnx
 
   file.write(
-  f"""
-func {model_name}_eval(model *{model_name}_model, threadCount int, digit []float32) int {{
+  f"""\
+func model_eval(model *model_struct, threadCount int, digit []float32) int {{
 
-	ctx0 := &ml.Context{{}}
-	graph := ml.Graph{{ThreadsCount: threadCount}}
+  ctx0 := &ml.Context{{}}
+  graph := ml.Graph{{ThreadsCount: threadCount}}
 
-	input := ml.NewTensor1D(ctx0, ml.TYPE_F32, uint32(model.hparams.n_input))
-	copy(input.Data, digit)
+  input := ml.NewTensor1D(ctx0, ml.TYPE_F32, uint32(model.hparams.n_input))
+  copy(input.Data, digit)
 
-	// fc1 MLP = Ax + b
-	fc1 := ml.Add(ctx0, ml.MulMat(ctx0, model.fc1_weight, input), model.fc1_bias)
-	fc2 := ml.Add(ctx0, ml.MulMat(ctx0, model.fc2_weight, ml.Relu(ctx0, fc1)), model.fc2_bias)
+{layers}
 
-	// softmax
-	final := ml.SoftMax(ctx0, fc2)
+  // run the computation
+  ml.BuildForwardExpand(&graph, final)
+  ml.GraphCompute(ctx0, &graph)
 
-	// run the computation
-	ml.BuildForwardExpand(&graph, final)
-	ml.GraphCompute(ctx0, &graph)
+  ml.PrintTensor(final, "final tensor")
 
-	ml.PrintTensor(final, "final tensor")
-
-	maxIndex := 0
-	for i := 0; i < 10; i++{{
-		if final.Data[i] > final.Data[maxIndex] {{
-			maxIndex = i
-		}}
-	}}
-	return maxIndex
+  maxIndex := 0
+  for i := 0; i < 10; i++{{
+    if final.Data[i] > final.Data[maxIndex] {{
+      maxIndex = i
+    }}
+  }}
+  return maxIndex
 }}
-  """
+
+"""
   )
 
-def create_main_func(file, model_name: str):
+def create_main_func(file):
   """
   create inference main function
   e.g. TestMNIST 
@@ -236,50 +234,50 @@ def create_main_func(file, model_name: str):
   file.write(
 f"""\
 func main() {{
-	modelFile := "{modelFile}"
-	digitFile := "{digitFile}"
+  modelFile := "{modelFile}"
+  digitFile := "{digitFile}"
 
-	ml.SINGLE_THREAD = true
-	model := new({model_name}_model)
-	if err := {model_name}_model_load(modelFile, model); err != nil {{
-		fmt.Println(err)
-		return
-	}}
+  ml.SINGLE_THREAD = true
+  model := new(model_struct)
+  if err := model_load(modelFile, model); err != nil {{
+    fmt.Println(err)
+    return
+  }}
 
-	// load a random test digit
-	fin, err := os.Open(digitFile)
-	if err != nil {{
-		fmt.Println(err)
-		return
-	}}
-	 // Seek to a random digit: 16-byte header + 28*28 * (random 0 - 10000)
-	rand.Seed(time.Now().UnixNano())
-	fin.Seek(int64(16 + 784 * (rand.Int() % 10000)), 0)
-	buf := make([]byte, 784)
-	digits := make([]float32, 784)
-	if count, err := fin.Read(buf); err != nil || count != int(len(buf)) {{
-		fmt.Println(err, count)
-		return
-	}}
+  // load a random test digit
+  fin, err := os.Open(digitFile)
+  if err != nil {{
+    fmt.Println(err)
+    return
+  }}
+   // Seek to a random digit: 16-byte header + 28*28 * (random 0 - 10000)
+  rand.Seed(time.Now().UnixNano())
+  fin.Seek(int64(16 + 784 * (rand.Int() % 10000)), 0)
+  buf := make([]byte, 784)
+  digits := make([]float32, 784)
+  if count, err := fin.Read(buf); err != nil || count != int(len(buf)) {{
+    fmt.Println(err, count)
+    return
+  }}
 
-	// render the digit in ASCII
-	for row := 0; row < 28; row++{{
-		for col := 0; col < 28; col++ {{
-			digits[row*28 + col] = float32(buf[row*28 + col])
-			var c string
-			if buf[row*28 + col] > 230 {{
-				c = "*"
-			}} else {{
-				c = "_"
-			}}
-			fmt.Printf(c)
-		}}
-		fmt.Println("")
-	}}
-	fmt.Println("")
+  // render the digit in ASCII
+  for row := 0; row < 28; row++{{
+    for col := 0; col < 28; col++ {{
+      digits[row*28 + col] = float32(buf[row*28 + col])
+      var c string
+      if buf[row*28 + col] > 230 {{
+        c = "*"
+      }} else {{
+        c = "_"
+      }}
+      fmt.Printf(c)
+    }}
+    fmt.Println("")
+  }}
+  fmt.Println("")
 
-	res := {model_name}_eval(model, 1, digits)
-	fmt.Println("Predicted digit is ", res)
+  res := model_eval(model, 1, digits)
+  fmt.Println("Predicted digit is ", res)
 }}
 """
   )
