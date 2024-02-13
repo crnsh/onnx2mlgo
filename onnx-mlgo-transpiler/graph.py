0461151eb0b530d21836a3a1c671aa9ff70e0872
Karan Handa <karan.handa.fox@gmail.com>
2024-02-13 19:32:25 +0530

    feat: add complete graph

--- onnx-mlgo-transpiler/graph.py
+++ onnx-mlgo-transpiler/graph.py
@@ -7,11 +7,11 @@ class Node:
     pass
   
   @classmethod
-  def create_node(cls, node):
+  def create_node(cls, node, i: int):
     if node.op_type == "Gemm":  
-      intermediate_ouput = name that is different from all others in inputs and outputs
-      node1 = Node('MulMat', node.input[0:2], intermediate_ouput)
-      node2 = Node('Add', [intermediate_ouput, node.input[2]], node.output)
+      temp_output = f'temp{i}'
+      node1 = Node('MulMat', node.input[0:2], temp_output)
+      node2 = Node('Add', [temp_output, node.input[2]], node.output)
       return [node1, node2]
     elif node.op_type == "Relu":
       pass
@@ -23,12 +23,20 @@ class Graph:
 
     # properties
     self._nodes: List[Node] = []
-    self._inputs = [] # type List[Tensor]
-    self._outputs = [] # type List[Tensor]
+    self._inputs = onnx_graph.graph.input # type List[Tensor]
+    self._outputs = onnx_graph.graph.output # type List[Tensor]
 
-    for node in onnx_graph.graph.node:
+    i = 1
+    for onnx_node in onnx_graph.graph.node:
       # assert : self._graph is a complete graph with last output as last_output
       # assert : node takes in 1 input, x weights and has 1 output
-      node_list = Node.create_node(node)
+      node_list = Node.create_node(onnx_node, i)
       
-      add node_list to graph
+      for node in node_list:
+        self.add_node_to_graph(node)
+      
+      i += 1
+  
+  def add_node_to_graph(self, node):
+    pass
+        
