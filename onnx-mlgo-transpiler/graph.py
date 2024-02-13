e64b4779c086e1ead5fc2addade737291e64d6e3
Karan Handa <karan.handa.fox@gmail.com>
2024-02-13 17:49:56 +0530

    chore: graph almost done

--- onnx-mlgo-transpiler/graph.py
+++ onnx-mlgo-transpiler/graph.py
@@ -2,13 +2,19 @@ from onnx.checker import check_model
 from typing import List
 
 class Node:
-  def __init__(self, id: str, op: str, *inputs: List[str], output: str, input_first: bool):
+  def __init__(self, node):
     # TODO: add other necessary params as well
-    self.id = id
-    self.op = op
-    self.inputs = inputs
-    self.output = output
-    self.input_first = input_first
+    pass
+  
+  @classmethod
+  def create_node(cls, node):
+    if node.op_type == "Gemm":  
+      intermediate_ouput = name that is different from all others in inputs and outputs
+      node1 = Node('MulMat', node.input[0:2], intermediate_ouput)
+      node2 = Node('Add', [intermediate_ouput, node.input[2]], node.output)
+      return [node1, node2]
+    elif node.op_type == "Relu":
+      pass
 
 class Graph:
   def __init__(self, onnx_graph):
@@ -16,11 +22,13 @@ class Graph:
     check_model(onnx_graph)
 
     # properties
-    graph: List[Node] = []
+    self._nodes: List[Node] = []
+    self._inputs = [] # type List[Tensor]
+    self._outputs = [] # type List[Tensor]
 
     for node in onnx_graph.graph.node:
-      pass
-
-
-  def in_order(self):
-    pass
+      # assert : self._graph is a complete graph with last output as last_output
+      # assert : node takes in 1 input, x weights and has 1 output
+      node_list = Node.create_node(node)
+      
+      add node_list to graph
