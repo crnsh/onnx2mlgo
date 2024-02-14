4d9f22a881f797fc3862e1849300ac6e5c4fd392
Karan Handa <karan.handa.fox@gmail.com>
2024-02-14 15:14:58 +0530

    chore: rename transpiler.py and remove redundant comments

--- onnx2mlgo/utils.py
+++ onnx2mlgo/utils.py
@@ -39,13 +39,4 @@ def create_layers(onnx) -> List[str]:
 
   # assert : output is the go language output for the layers
   
-#   output = """\
-#   // fc1 MLP = Ax + b
-#   fc1 := ml.Add(ctx0, ml.MulMat(ctx0, model.fc1_weight, input), model.fc1_bias)
-#   fc2 := ml.Add(ctx0, ml.MulMat(ctx0, model.fc2_weight, ml.Relu(ctx0, fc1)), model.fc2_bias)
-
-#   // softmax
-#   final := ml.SoftMax(ctx0, fc2)
-# """
-  
   return output
